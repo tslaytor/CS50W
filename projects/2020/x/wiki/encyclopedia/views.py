f9b2f41c7751709b9dbed15272b0f6795dc3ca46
Tom <tom.slaytor@gmail.com>
2022-02-02 16:43:52 +0200

    completed the SEARCH specifications, including redirecting to results, listing matches for substring in string, and linking to the entry page from list of results

--- projects/2020/x/wiki/encyclopedia/views.py
+++ projects/2020/x/wiki/encyclopedia/views.py
@@ -1,22 +1,57 @@
-from django.shortcuts import render
+from pickletools import read_uint1
+import re
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django import forms
 from . import util
 
+class NewSearchForm(forms.Form):
+    query = forms.CharField(label="query")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
     })
 
 def entry(request, entry):
     # return HttpResponse(f"Hello {entry}")
-    content = util.get_entry(entry)
-    if content:
-        return render(request, f"encyclopedia/entry.html", {
-            "content": content,
-            "title": entry.capitalize()
-        })
+    if request.method == 'GET':
+        content = util.get_entry(entry)
+        if content:
+            return render(request, f"encyclopedia/entry.html", {
+                "content": content,
+                "title": entry.capitalize(),
+                "form": NewSearchForm()
+            })
+        else:
+            return render(request, "encyclopedia/no-entry.html", {
+                "title": entry.capitalize(),
+                "form": NewSearchForm()
+            })
     else:
-        return render(request, "encyclopedia/no-entry.html", {
-            "title": entry.capitalize()
-        })
+        return HttpResponse("hello mate - this is an http response")
+
+def search(request):
+    input = NewSearchForm(request.GET)
+    if input.is_valid():
+        input = input.cleaned_data["query"]
+        entry = util.get_entry(input)
+        if entry:
+            return redirect(f"wiki/{input}")
+        else:
+            # look for substrings of {input} in all entries and return list of matching entries
+            entries = util.list_entries()
+            matches = []
+            for title in entries:
+                if input in title:
+                    matches.append(title)
+            if matches:
+                return render(request, "encyclopedia/search.html", {
+                    "entries": matches,
+                    "form": NewSearchForm()
+                    })
+            else:
+                return HttpResponse("NO MATCHES")
+    else:
+        return HttpResponse("Invalid form!")
