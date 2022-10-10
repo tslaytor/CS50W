2c82c03138dc7bec96ce39d52570f58c83325793
Tom <tom.slaytor@gmail.com>
2022-10-10 22:44:18 +0300

    displaying emails in the mailbox view

--- projects/2020/x/mail/mail/static/mail/inbox.js
+++ projects/2020/x/mail/mail/static/mail/inbox.js
@@ -20,9 +20,61 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // add event listener to send button
+  document.querySelector('form').onsubmit = send_mail;
+  
+
+
+  // when you click send, send the recepients, subject and body in a post request to the /emails route
+  function send_mail() {
+    var recipients = document.querySelector('#compose-recipients').value
+    var subject = document.querySelector('#compose-subject').value
+    var body = document.querySelector('#compose-body').value
+
+    var data = {
+      'recipients': recipients,
+      'subject': subject,
+      'body': body
+    }
+
+    fetch('emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch((error) => {
+      console.log('error', error)
+    })
+    load_mailbox('sent')
+
+    return false;
+  }
 }
 
 function load_mailbox(mailbox) {
+
+   // make a request to get sent emails
+   fetch(`emails/${mailbox}`)
+   .then(response => response.json())
+   .then(function(data) {
+     for(let i = 0; i < data.length; i++){
+       console.log('yo')
+       let email = document.createElement('div');
+       email.className = 'email'
+       email.innerHTML = `<h6>Sender: ${data[i].sender}</h6>
+                           <h6>Subject: ${data[i].subject}</h6>
+                           <h6>Time: ${data[i].timestamp}</h6>`
+       document.querySelector('#emails-view').append(email)
+       
+     }
+   })
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
@@ -30,4 +82,8 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+ 
 }
+  
+
