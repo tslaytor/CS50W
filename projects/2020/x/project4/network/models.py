35d7859fe89646c8803b3f0291de6c01f4c8e668
Tom <tom.slaytor@gmail.com>
2022-10-23 15:59:19 +0300

    added new post form to index page and created models

--- projects/2020/x/project4/network/models.py
+++ projects/2020/x/project4/network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+
+class followers(models.Model):
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_query_name='follower', on_delete=models.CASCADE)
