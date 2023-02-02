e9a8a3b32b2b09eabbb97aedb80463850b3bcee2
Tom <tom.slaytor@gmail.com>
2023-02-02 13:07:33 +0200

    simplified some of the code, editing beams, implemented starting top beam only from first note of beat

--- projects/2020/x/capstone/drum_exercises/static/drum_exercises/drum_exercises.js
+++ projects/2020/x/capstone/drum_exercises/static/drum_exercises/drum_exercises.js
@@ -1,76 +1,74 @@
-// let bar_value_array = [
-//     [[3],[5]], 
-//     [[2,2],[4]]
-// ];
+
 let bar_value_array = [
-    [[{value: 4, rest: false}],[{value: 4, rest: true}]], 
+    [[{value: 4, rest: true}],[{value: 4, rest: false}]], 
+    [[{value: 2, rest: false},{value: 2, rest: false}],[{value: 4, rest: false}]],
+    [[{value: 4, rest: true}],[{value: 4, rest: false}]], 
     [[{value: 2, rest: false},{value: 2, rest: false}],[{value: 4, rest: false}]]
 ];
 document.addEventListener("DOMContentLoaded", function(){
+     // *************** CHANGES SOON TO HOW WE MAKE TEMPLATES? ********************************
+    // GET EVERY TEMPLATE NOTE IN THE DOM AND ADD THE REST ELEMENT NEXT TO IT 
+    let template_notes = Array.from(document.querySelectorAll('.note'));
+    template_notes.forEach(ele => ele.innerHTML += `<img class='eighthRest' src='../../static/drum_exercises/images/EigthNoteRest.png'>`);
+    // ***************************************************************************************
 
     // GET EVERY TEMPLATE BEAT IN THE DOM
     let template_beats = Array.from(document.querySelectorAll('.beat'));
-    let template_heads = Array.from(document.querySelectorAll('.head'));
-    let template_rests = Array.from(document.querySelectorAll('img'));
-    console.log('note heads')
-    console.log(template_heads)
 
-    let template_notes = Array.from(document.querySelectorAll('.note'));
-    template_notes.forEach(ele => ele.innerHTML += `<img class='eighthRest' src='../../static/drum_exercises/images/EigthNoteRest.png'>`);
-
-    // FOR EACH BEAT IN THE VALUE ARRAY, USE THE INDEX OF THE LOOP TO INDENTIFY WHICH TEMPLATE BEATS IT REFERS TO
+    // FOR EACH BEAT IN THE VALUE ARRAY
     bar_value_array.forEach(function(beat_value_array, index){
-        
-        // FIND THE CHILDREN OF THE IDENTIFIED TEMPLATE BEAT AND FILTER SO WE ONLY HAVE TEMPLATE SUBBEAT ELEMENTS
-        template_subbeats = Array.from(template_beats[index].children).filter(function (child) {
-            return child.classList.contains('sub-beat');
-        })
-
-        // CREATE EMPTY ARRAY FOR SAVING TEMPLATE NOTES
-        var template_notes_of_this_beat = [];
-        // LOOP THROUGH THE 2 TEMPLATE SUBBEATS, FIND THE CHILDREN, AND FILTER FOR ONLY THE TEMPLATE NOTES
-        template_subbeats.forEach(child =>{
-            var template_notes = Array.from(child.children).filter(child => {return child.classList.contains('note')});
-            template_notes_of_this_beat.push(template_notes);
-        });
-        // MERGE TEMPLATE NOTES INTO ONE ARRAY
-        template_notes_of_this_beat = [].concat(...template_notes_of_this_beat)
-
-        let template_rests_of_this_beat = template_notes_of_this_beat.reduce((acc, noteElement) => {
-            const imgElements = [...noteElement.querySelectorAll('img')];
-            return [...acc, ...imgElements];
-        }, []);
-
-        console.log('template_rests_of_this_beat')
-        console.log(template_rests_of_this_beat)
+        var beam_to_be_edited = getTopBeamForThisBeat(template_beats, index);
+        // GET THE TEMPLATE NOTES
+        var template_notes_of_this_beat = getTemplateNotesOfBeat(template_beats, index);
+        var note_obj_of_this_beat = [];
+        var indexs_of_non_rest_note_values = [];
 
         // SET A CURSOR TO KEEP TRACK OF WHICH NOTE IN THE TEMPLATE WE ARE EDITING
         let cursor = 0;
         // ITERATE OVER EACH SUBBEAT VALUE ARRAY AND EACH NOTE VAULE WITHIN
         beat_value_array.forEach(function(subbeat_value_array){
             subbeat_value_array.forEach(function(note_obj){
-                // FIRST, LETS GET THE RESTS IN THIS SUB
-                // WE CREATE A LOOP THE LENGTH OF THE CURRENT NOTE VALUE, AND USE THE VALUE OF THE CURSOR TO INDEX INTO THE TEMPLATE NOTES OF THIS BEAT TO SELECT WHICH NOTE WE WANT TO CHAGE
-                // IF IT IS THE FIRST NOTE (I.E. THE VALUE OF i IS 0) WE LEAVE IT VISABLE, OTHERWISE, WE HIDE IT WITH DISPLAY = NONE
+                 
+                // WE USE THE VALUE OF THE CURSOR TO INDEX INTO THE TEMPLATE NOTES AND SELECT WHICH NOTE WE WANT TO EDIT
+                // IF IT IS THE FIRST NOTE TEMPLATE NOTE OF THE LOOP WE LEAVE IT VISABLE, OTHERWISE, WE HIDE IT WITH DISPLAY = NONE
+                // CREATE A LOOP THE LENGTH OF THE CURRENT NOTE VALUE,
                 for (var i = 0; i < note_obj['value']; i++){
+                    // IF NOTE IS A REST, HIDE ALL ELEMENTS IN THE NOTE AND DISPLAY THE REST
                     if (note_obj['rest'] === true){
-                        var ele = template_notes_of_this_beat[cursor].children;
+                        var thisTemplateNote = template_notes_of_this_beat[cursor];
+                        var ele = thisTemplateNote.children;
                         Array.from(ele).forEach(el => el.style.display = 'none');
-                        template_notes_of_this_beat[cursor].querySelector('.eighthRest').style.display = 'block';
-                        console.log('ele')
-                        console.log(ele)
-                        console.log('template_rests_of_this_beat[cursor]');
-                        console.log(template_rests_of_this_beat[cursor]);
-                        // template_rests_of_this_beat[cursor].style.display = 'block';
+                        thisTemplateNote.querySelector('.eighthRest').style.display = 'block';
+                        
                     }
+                    // OTHERWISE, SAVE THE POSITION OF THE NOTE FOR BEAM EDITING
+                    else if (i === 0){
+                        indexs_of_non_rest_note_values.push(cursor)
+                    }
+
                     if (i > 0){
                         var ele = template_notes_of_this_beat[cursor].children;
                         Array.from(ele).forEach(el => el.style.display = 'none');
                     }
                     cursor++;
                 }
+                note_obj_of_this_beat.push(note_obj);
             });
         })
+        console.log('indexs_of_non_rest_note_values');
+        console.log(indexs_of_non_rest_note_values);
+
+        topBeamEditor(indexs_of_non_rest_note_values, beam_to_be_edited);
+
+        // to calculate where the top beam starts and ends, find the first and last note of the beat
+        // var indexs_of_non_rest_note_values = [];
+        // note_obj_of_this_beat.forEach(note_obj, i => {
+        //     if (!note_obj['rest']){
+        //         indexs_of_non_rest_note_values.push(i)
+        //     }
+        // })
+
+
         // here we can determine: 
         // 1. whether the top beam is visible 
         // 2. which notes belong to this beat
@@ -93,10 +91,37 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 
-function topBeamChanger(){
-    // where is starts (always 0 from now)
-    
-    
-    // where is ends
+function getTemplateNotesOfBeat(templateBeats, index){
+    // GET THE TEMPLATE SUBBEAT ELEMENTS FOR THE IDENTIFIED TEMPLATE BEAT
+        var templateSubbeats = Array.from(templateBeats[index].children).filter(function (child) {
+        return child.classList.contains('sub-beat');
+    })
 
+    // CREATE EMPTY ARRAY FOR SAVING TEMPLATE NOTES
+    var templateNotesOfThisBeat = [];
+
+    // LOOP THROUGH THE 2 TEMPLATE SUBBEATS, AND GET ITS TEMPLATE NOTES
+    templateSubbeats.forEach(child =>{
+        var templateNotes = Array.from(child.children).filter(child => {return child.classList.contains('note')});
+        templateNotesOfThisBeat.push(templateNotes);
+    });
+    // MERGE TEMPLATE NOTES OF THE 2 SUBBEATS INTO ONE ARRAY
+    return [].concat(...templateNotesOfThisBeat)
+
+}
+
+function getTopBeamForThisBeat(templateBeats, index){
+    // GET THE TEMPLATE SUBBEAT ELEMENTS FOR THE IDENTIFIED TEMPLATE BEAT
+    var topBeam = Array.from(templateBeats[index].children).filter(function (child) {
+        return child.classList.contains('top-beam');
+    })
+    return topBeam[0];
+}
+
+function topBeamEditor(indexs_of_non_rest_note_values, beam_to_be_edited){
+    console.dir(beam_to_be_edited);
+    var first_and_last = [indexs_of_non_rest_note_values[0], indexs_of_non_rest_note_values[indexs_of_non_rest_note_values.length - 1]];
+    console.log(first_and_last)
+    beam_to_be_edited.style.transform = `translateX(${first_and_last[0] * 14}px)`
+    beam_to_be_edited.style.borderTop = '8px solid pink'
 }
