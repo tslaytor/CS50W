b31b9197527d857607fd07fb8c6d63a3683fdcfd
Tom <tom.slaytor@gmail.com>
2023-02-20 15:21:01 +0200

    deleted old code that was commented, fixed use of let and var to be appropriate for case

--- projects/2020/x/capstone/drum_exercises/static/drum_exercises/drum_exercises.js
+++ projects/2020/x/capstone/drum_exercises/static/drum_exercises/drum_exercises.js
@@ -50,70 +50,7 @@ let bars = [
     }
 ]
 
-
 document.addEventListener("DOMContentLoaded", function(){
-
-    // // FOR EACH BEAT IN THE VALUE ARRAY
-    // bars.forEach( bar => {
-    //     bar["barValueArray"].forEach(function(beat_value_array, index){
-        
-    //         // GET THE TEMPLATE NOTES
-    //         var template_notes_of_this_beat = getTemplateNotesOfBeat(template_beats, index);
-    //         var note_obj_of_this_beat = [];
-    //         var indexs_of_non_rest_note_values = [];
-    
-    //         // SET A CURSOR TO KEEP TRACK OF WHICH NOTE IN THE TEMPLATE WE ARE EDITING
-    //         let cursor = 0;
-    //         // ITERATE OVER EACH SUBBEAT VALUE ARRAY AND EACH NOTE VAULE WITHIN
-    //         beat_value_array.forEach(function(subbeat_value_array){
-    //             subbeat_value_array['notes'].forEach(function(note_obj){
-                     
-    //                 // WE USE THE VALUE OF THE CURSOR TO INDEX INTO THE TEMPLATE NOTES AND SELECT WHICH NOTE WE WANT TO EDIT
-    //                 // IF IT IS THE FIRST NOTE TEMPLATE NOTE OF THE LOOP WE LEAVE IT VISABLE, OTHERWISE, WE HIDE IT WITH DISPLAY = NONE
-    //                 // CREATE A LOOP THE LENGTH OF THE CURRENT NOTE VALUE,
-    //                 for (var i = 0; i < note_obj['value']; i++){
-
-    //                     // IF NOTE IS A REST, HIDE ALL ELEMENTS IN THE NOTE AND DISPLAY THE REST
-    //                     if (note_obj['rest'] === true){
-    //                         var thisTemplateNote = template_notes_of_this_beat[cursor];
-    //                         var ele = thisTemplateNote.children;
-    //                         Array.from(ele).forEach(el => el.style.display = 'none');
-    //                         thisTemplateNote.querySelector('.eighthRest').style.display = 'block';
-    //                     }
-
-    //                     // OTHERWISE, SAVE THE POSITION OF THE NOTE FOR BEAM EDITING
-    //                     if (i === 0){
-    //                         indexs_of_non_rest_note_values.push({'cursor': cursor, 'value': note_obj['value'], 'rest': note_obj['rest']});
-    //                     }
-    
-    //                     if (i > 0){
-    //                         var ele = template_notes_of_this_beat[cursor].children;
-    //                         Array.from(ele).forEach(el => el.style.display = 'none');
-    //                     }
-    //                     cursor++;
-    //                 }
-    //                 note_obj_of_this_beat.push(note_obj);
-    //             });
-    //         })
-    //         // console.log('note_obj_of_this_beat');
-    //         // console.log(note_obj_of_this_beat);
-    
-    
-    
-    //         var top_beam_to_be_edited = getBeamForThisBeat(template_beats, index, 'top-beam');
-    //         topBeamEditor(indexs_of_non_rest_note_values, top_beam_to_be_edited);
-    
-    //         var middle_beam_to_be_edited = getBeamForThisBeat(template_beats, index, 'middle-beam');
-    
-    //                 // I CAN CHECK HOW MANY MIDDLE BEAMS ON THIS BEAT WITH THE NOTE OBJ OF THIS BEAT ARRAY
-    //         // IF THERE ARE AT LEAST 2 PLAYED NOTES WITH A VALUE OF 16TH (2) OR LESS 
-    //         // AND THAT HAS ANY REST IN BETWEEN THEM, OR A PLAYED NOTE WITH A VALUE OF 8TH (4) OR GREATER
-    //         var x = middleBeamEditor(indexs_of_non_rest_note_values, middle_beam_to_be_edited);
-    //         console.log('how many beams?');
-    //         console.log(x);
-    
-    //     })
-    // })
 
     // make a div inside container
     let newContainer = makeElement('container', document.querySelector('body'), 'div')
@@ -127,78 +64,54 @@ document.addEventListener("DOMContentLoaded", function(){
             let leftOvers = 0;
             let cursor = 0;
             let indexs_of_non_rest_note_values = [];
-            let topBeam = null;
 
             beat.forEach( subbeat => {
 
                 let subdiv = subbeat['subdivision'];
-
-
                 let newSubBeat = makeElement('sub-beat', newBeat, 'div')
 
                 subbeat['notes'].forEach( note => {
                     // add any empty notes from previous sub-beat
-                    for (var i = 0; i < leftOvers; i++){
+                    for ( var i = 0; i < leftOvers; i++ ){
                         makeElement('note', newSubBeat, 'div');
                         cursor++;
                     }
                     
                     for ( var i = 0; i < note['value'] && i < subdiv; i++ ){
-                        var newNote = document.createElement('div');
+                        let newNote = document.createElement('div');
                         newNote.className = 'note';
                         newSubBeat.appendChild(newNote);
 
                         if (i === 0) {
-
-                            // IF NOTE IS A REST, HIDE ALL ELEMENTS IN THE NOTE AND DISPLAY THE REST
                             if (note['rest']){
-                                // var thisTemplateNote = template_notes_of_this_beat[cursor];
-                                // var ele = thisTemplateNote.children;
-                                // Array.from(ele).forEach(el => el.style.display = 'none');
-                                // template_notes.forEach(ele => ele.innerHTML += `<img class='eighthRest' src='../../static/drum_exercises/images/EigthNoteRest.png'>`);
-
-                                // makeElement
-                                // make a dive with class of .eigthRest in the newNote div
                                 makeElement('eighthRest', newNote, 'img');
-                                // thisTemplateNote.querySelector('.eighthRest').style.display = 'block';
                             }
                             else {
-                                var newHead = document.createElement('div');
+                                let newHead = document.createElement('div');
                                 newHead.className = 'head';
-                                var newStem = document.createElement('div');
+                                let newStem = document.createElement('div');
                                 newStem.className = 'stem';
                                 newNote.appendChild(newHead);
                                 newNote.appendChild(newStem);
-                            }
-
-
-                     
-
-                            if ( !note['rest']) {
                                 indexs_of_non_rest_note_values.push({'cursor': cursor, 'value': note['value'], 'rest': note['rest']});
-                            };  
-                        };
+                            }
+                        }
                         cursor++;
                     };
-
                     subdiv = subdiv - i;
                     leftOvers = note['value'] - i;
                 });
                 
             });
-            // if (topBeam) {
-                topBeamEditor(indexs_of_non_rest_note_values, newBeat);
-            // };
-
+            topBeamEditor(indexs_of_non_rest_note_values, newBeat);
             middleBeamEditor(indexs_of_non_rest_note_values, newBeat);
-
         });    
     });    
 });
 
 // makes a div element with class name and appended to the parent
 function makeElement(nameOfClass, parentElement, type) {
-    var newElement = document.createElement(type);
+    let newElement = document.createElement(type);
     newElement.className = nameOfClass;
     parentElement.appendChild(newElement);
 
@@ -214,58 +127,16 @@ function makeTopBeam(thisBeat){
     return topBeam;
 };
 
-// function getTemplateNotesOfBeat(templateBeats, index){
-//     // GET THE TEMPLATE SUBBEAT ELEMENTS FOR THE IDENTIFIED TEMPLATE BEAT
-//         var templateSubbeats = Array.from(templateBeats[index].children).filter(function (child) {
-//         return child.classList.contains('sub-beat');
-//     })
-
-//     // CREATE EMPTY ARRAY FOR SAVING TEMPLATE NOTES
-//     var templateNotesOfThisBeat = [];
-
-//     // LOOP THROUGH THE 2 TEMPLATE SUBBEATS, AND GET ITS TEMPLATE NOTES
-//     templateSubbeats.forEach(child =>{
-//         var templateNotes = Array.from(child.children).filter(child => {return child.classList.contains('note')});
-//         templateNotesOfThisBeat.push(templateNotes);
-//     });
-//     // MERGE TEMPLATE NOTES OF THE 2 SUBBEATS INTO ONE ARRAY
-//     return [].concat(...templateNotesOfThisBeat)
-
-// }
-
-// function getBeamForThisBeat(templateBeats, index, beamName){
-//     // GET THE TEMPLATE SUBBEAT ELEMENTS FOR THE IDENTIFIED TEMPLATE BEAT
-//     var beam = Array.from(templateBeats[index].children).filter(function (child) {
-//         return child.classList.contains(beamName);
-//     })
-//     return beam[0];
-// }
-
-
 function topBeamEditor(indexs_of_non_rest_note_values, parentBeat){
-    console.log('indexs_of_non_rest_note_values');
-    console.log(indexs_of_non_rest_note_values);
-
-    // ****************************************************************
-
-    // if (note['value'] < 8) {
-    //     // makeTopBeam() makes only one top beam per beat, even with multiple calls
-    //     topBeam = makeTopBeam(newBeat);
-    // }
-
-    // ****************************************************************
     
     let non_rests = indexs_of_non_rest_note_values.filter(obj => !obj['rest'])
-    var first_and_last = [non_rests[0]['cursor'], non_rests[non_rests.length - 1]['cursor']];
+    let first_and_last = [non_rests[0]['cursor'], non_rests[non_rests.length - 1]['cursor']];
 
-    let topBeam = null;
-    non_rests.forEach(note => {
-        if (note['value'] < 8) {
-            topBeam = makeTopBeam(parentBeat);
+    for (let i = 0; i < non_rests.length; i++) {
+        if (non_rests[i]['value'] < 8) {
+            var topBeam = makeTopBeam(parentBeat);
         }
-    })
-    console.log('first_and_last');
-    console.log(first_and_last);
+    }
     // EDIT THE LENGTH OF THE BEAM
     if (topBeam){
         topBeam.style.width = `${(first_and_last[1] - first_and_last[0]) * NOTEWIDTH}px`
@@ -281,45 +152,11 @@ function topBeamEditor(indexs_of_non_rest_note_values, parentBeat){
     }
 }
 
-// I THINK I NEED TO PASS THE BEAM TO BE EDITED
-// function middleBeamEditor(indexs_of_non_rest_note_values, beamToBeEdited){
-//     let non_rests = indexs_of_non_rest_note_values.filter(obj => !obj['rest'] && obj['value'] <= 2)
-//     var array16thPositions = [];
-//     var array8thAndRestPositions = [];
-//     var num_of_beams = 1;
-//     indexs_of_non_rest_note_values.forEach(function(note_obj, index){
-//         if (note_obj['value'] <= 2 && !note_obj['rest']){
-//             array16thPositions.push(note_obj['cursor']);
-//         }
-//         else if (note_obj['value'] >= 4 || note_obj['rest']){
-//             array8thAndRestPositions.push(note_obj['cursor']);
-//         }
-//     })
-//     let length = array16thPositions.length;
-//     if (length === 0){
-//         num_of_beams = 0;
-//         // in this case, I need to hide the beam
-//         beamToBeEdited.style.display = 'none';
-//         return num_of_beams;
-//     }
-//     else {
-//         for(var i = 0; i < array8thAndRestPositions.length; i++){
-//             if (array16thPositions[0] < array8thAndRestPositions[i] && array8thAndRestPositions[i] < array16thPositions[length - 1]){
-//                 num_of_beams = 2;
-//             }
-//         }
-//         beamToBeEdited.style.width = `${(non_rests[non_rests.length - 1]['cursor'] - non_rests[0]['cursor']) * NOTEWIDTH}px`
-//         beamToBeEdited.style.transform = `translateX(${non_rests[0]['cursor'] * NOTEWIDTH}px)`;
-//         return num_of_beams;
-//     }    
-// }
-
-
 function middleBeamEditor(indexs_of_non_rest_note_values, parentBeat){
     let non_rests = indexs_of_non_rest_note_values.filter(obj => !obj['rest'] && obj['value'] <= 2)
-    var array16thPositions = [];
-    var array8thAndRestPositions = [];
-    var num_of_beams = 1;
+    let array16thPositions = [];
+    let array8thAndRestPositions = [];
+    let num_of_beams = 1;
     indexs_of_non_rest_note_values.forEach(function(note_obj, index){
         if (note_obj['value'] <= 2 && !note_obj['rest']){
             array16thPositions.push(note_obj['cursor']);
@@ -334,7 +171,7 @@ function middleBeamEditor(indexs_of_non_rest_note_values, parentBeat){
         return 0;
     }
     else {
-        for(var i = 0; i < array8thAndRestPositions.length; i++){
+        for ( let i = 0; i < array8thAndRestPositions.length; i++ ){
             if (array16thPositions[0] < array8thAndRestPositions[i] && array8thAndRestPositions[i] < array16thPositions[length - 1]){
                 // num_of_beams = 2;
                 // no... make 2 beams and append them to the parent
